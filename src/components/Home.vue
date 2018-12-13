a1f876bc6ee4e3452423449e2f84750a16306fa0
xiyanjo <1447656442@qq.com>
2018-12-13 11:26:07 +0800

    '无法启动问题'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -144,14 +144,14 @@
     import {mapState, mapMutations, mapGetters, mapActions} from 'vuex';//辅助函数帮助我们生成计算属性
 
     // 使用 Mock
-    var Mock = require('mockjs')
-    var data = Mock.mock({
-        // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-        'list|1-10': [{
-            // 属性 id 是一个自增数，起始值为 1，每次增 1
-            'id|+1': 1
-        }]
-    })
+    // var Mock = require('mockjs')
+    // var data = Mock.mock({
+    //     // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    //     'list|1-10': [{
+    //         // 属性 id 是一个自增数，起始值为 1，每次增 1
+    //         'id|+1': 1
+    //     }]
+    // })
     var partChild = {
         template: '<div>我是局部组件，需要components引入</div>'
     }
